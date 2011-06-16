76a75869122e924f4d3ffc64d4f9fe9036651cbb
Chanwit Kaewkasi <chanwit@gmail.com>
2011-06-16 15:46:16 +0700

    change mongo driver to mgo. update hello controller

--- app/controller/hello.go
+++ app/controller/hello.go
@@ -1,7 +1,19 @@
 package hello
 
 import . "framework/mv"
+import  "launchpad.net/gobson/bson"
+import  "launchpad.net/mgo"
+
+func findById(c mgo.Collection, id string) Model {
+	m := Model{}
+	_ = c.Find(bson.M{"_id": id}).One(&m)	
+	return m
+}
 
 func Index() Model {
-    return Model{"name":"Golang at SUT"}
+	session, _ := mgo.Mongo("127.0.0.1")
+	session.SetMode(mgo.Monotonic, true)
+	defer session.Close()
+	book := session.DB("gon").C("book")
+    return findById(book, "doc1")
 }
--- app/view/hello/index.m
+++ app/view/hello/index.m
@@ -1,6 +1,8 @@
 <html>
     <body>
-        Hello, {{name}} <br/>
-        This is test.
+        Hello, MongoDB and Gon<br/>
+        Id: {{_id}} <br/>
+        Title: {{title}} <br/>
+		Content: {{content}}
     </body>
 </html>
