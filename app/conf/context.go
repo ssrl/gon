4c190f7bab43106c61e5fe718263ac62344b7b46
Chanwit Kaewkasi <chanwit@gmail.com>
2011-06-21 21:13:33 +0700

    formatting codes

--- app/conf/context.go
+++ app/conf/context.go
@@ -16,30 +16,30 @@ type Context struct {
 
 var ch chan *Context = make(chan *Context, 1)
 func StartBeanServer() {
-  go func(){
-    for {
-      ctx := <-ch
-      registry[ctx.name] = ctx.function
-      ctx.reply<- true
-    }
-  }()
+    go func(){
+        for {
+            ctx := <-ch
+            registry[ctx.name] = ctx.function
+            ctx.reply<- true
+        }
+    }()
 }
 
 func bean(name string, f func()interface{}) {
-  ctx := &Context{name, f, make(chan bool,1)}
-  ch<- ctx
-  <-ctx.reply
+    ctx := &Context{name, f, make(chan bool,1)}
+    ch<- ctx
+    <-ctx.reply
 }
 
 func GetBean(name string) interface{} {
-	return registry[name]()
+    return registry[name]()
 }
 
 func Init() {
-  StartBeanServer()  
-	bean("Session", func()interface{} {
-		session, _ := mgo.Mongo("127.0.0.1")
-		session.SetMode(mgo.Monotonic, true)
-		return session
-	})
+    StartBeanServer()  
+    bean("Session", func()interface{} {
+        session, _ := mgo.Mongo("127.0.0.1")
+        session.SetMode(mgo.Monotonic, true)
+        return session
+    })
 }
