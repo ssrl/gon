1d61b4ba5ec5641055fc4765659e6de2ff156078
Chanwit Kaewkasi <chanwit@gmail.com>
2011-06-28 00:10:40 +0700

    uncomment broken mapping

--- app/conf/url_mappings.go
+++ app/conf/url_mappings.go
@@ -1,6 +1,6 @@
 package mapping
 
 func init() {
-    Map{url:"/",controller:"hello"}
-    Map{url:"/$controller/$action/$id?"}
+    // Map{url:"/",controller:"hello"}
+    // Map{url:"/$controller/$action/$id?"}
 }
