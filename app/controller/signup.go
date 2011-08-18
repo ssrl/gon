f108b74977fd556cc6c92e86c6cfd73fbfd8e02d
wingyplus <wingyminus@gmail.com>
2011-08-19 02:03:21 +0700

    run web PASS

--- app/controller/signup.go
+++ app/controller/signup.go
@@ -3,6 +3,7 @@ package signup
 import . "framework/mv"
 import mymy "github.com/ziutek/mymysql"
 import "gaz"
+import "dump"
 
 type SignupController struct {
     Params   map[string]string
@@ -10,8 +11,9 @@ type SignupController struct {
 }
 
 func (c *SignupController) Index() Model {
-    col := c.Connection.DB("test").C("user")
-    _, _ = col.Insert(&map[string]string{"name": c.Params["name"], "password": c.Params["email"], "email": c.Params["email"]})
+    col := new(gaz.Connection).DB("test").C("User")
+	dump.Dump(col)
+    _, _ = col.Insert(map[string]string{"name": c.Params["name"], "email": c.Params["email"], "password": "1234"})
     result := make(map[string]string)
     data := col.FindOne(gaz.Params{"name": c.Params["name"]}).(*mymy.Row)
 	result["name"] = data.Str(1)
